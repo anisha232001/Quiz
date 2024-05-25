a09c39d48948c198eb718e47f8cbf8e5788b147c
anisha232001 <153667216+anisha232001@users.noreply.github.com>
2024-05-26 03:16:56 +0530

    uploading files

---
+++ src/helpers/Questions.js
@@ -0,0 +1,80 @@
+export const Questions = [
+    {
+      prompt: "What is full form of css?",
+      optionA: "cascading-sheet",
+      optionB: "cascading style ",
+      optionC: "style",
+      optionD: "cascading style sheet",
+      asnwer: "optionD",
+    },
+    {
+      prompt: "Which of the following command is used to create react-js-app ?",
+      optionA: "npx create-react-app appname",
+      optionB: "npm install create-react-app",
+      optionC: "npx install create-react-app -g",
+      optionD: "install - l create-react-app",
+      asnwer: "optionA",
+    },
+    {
+      prompt: "In React.js which one of the following is used to create a class for Inheritance ?",
+      optionA: "Create",
+      optionB: "extends",
+      optionC: "inherits",
+      optionD: "delete",
+      asnwer: "optionB",
+    },
+   {
+    prompt:"What is the default port number in which the application run ?",
+optionA:"3000",
+optionB:"8080",
+optionC:"5000",
+optionD:"3030",
+asnwer:"optionA"
+   },
+   {
+   prompt:" Which of the following valid component return type of React ?",
+optionA:"2",
+optionB:"5",
+optionC:"1",
+optionD:"3",
+asnwer:"optionC"
+   },
+   {
+    prompt:"Which of the following is a way to handle data in React.js ?",
+    optionA:"State & Props",
+    optionB:"Services & Components",
+    optionC:"State & Services",
+    optionD:"State & Component",
+  asnwer:"optionA"
+  },
+   {
+    prompt:"Which of the following is must for the API in React.js ?",
+    optionA:"SetinitialComponent",
+    optionB:"renderComponent",
+    optionC:"render",
+    optionD:"All of the above",
+    asnwer:"optionB"
+    },
+   {
+    prompt:"Which of the following is true regarding Babel ?",
+    optionA:"Compiler",
+    optionB:"Transpilar",
+    optionC:"Both of the above",
+   optionD:" None of the above",
+  asnwer:"optionC"},
+   {
+    prompt:"In React.js, how we can pass the data from one component to another in React.js ?",
+    optionA:"SetState",
+    optionB:"Render with arguments",
+    optionC:"Props",
+    optionD:"PropTypes",
+  asnwer:"optionC"},
+   {
+    prompt:"Which of the following method is true about referring parent class in React.js ?",
+    optionA:"self()",
+    optionB:"inherits()",
+    optionC:"this()",
+    optionD:"super()",
+  asnwer:"optionD"}
+  ];
+  
